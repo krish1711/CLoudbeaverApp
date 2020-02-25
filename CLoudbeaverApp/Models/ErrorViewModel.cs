8b063c711b08c349ed4b00f86f97cb8413266d0f
kishore Chowdary <lovelykishore02@outlook.com>
2020-02-25 11:38:57 +0530

    Add project files.

---
+++ CLoudbeaverApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CLoudbeaverApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
